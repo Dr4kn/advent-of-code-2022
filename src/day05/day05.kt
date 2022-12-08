2a4a3880f283b136ab50a85b44291598e9b600b4
Dr4kn <servicesDrkn@gmail.com>
2022-12-08 20:01:55 +0100

    day05 setup

--- src/day05/day05.kt
+++ src/day05/day05.kt
@@ -1,5 +1,8 @@
 fun main() {
     fun part1(input: List<String>): String {
+        // cargo
+        // cargoCrane
+
         return "CMZ"
     }
 
@@ -9,7 +12,8 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
-    check(part1(testInput) == "CMZ")
+    println(part1(testInput))
+//    check(part1(testInput) == "CMZ")
 
     val input = readInput("Day05")
     println(part1(input))
