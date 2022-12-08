199a4410296fb8420b4650af92be8e840baf3e50
Dr4kn <servicesDrkn@gmail.com>
2022-12-08 05:55:02 +0100

    day 3 done

--- src/day03/Day03.kt
+++ src/day03/Day03.kt
@@ -38,15 +38,43 @@ fun main() {
         return sum
     }
 
+    fun findBadge(backpacks: Array<List<Char>>): Char {
+        for (badge1 in backpacks[0]) {
+            for (badge2 in backpacks[1]) {
+                if (badge1 != badge2) continue
+
+                for (badge3 in backpacks[2]) {
+                    if (badge1 == badge3) {
+                        return badge1
+                    }
+                }
+            }
+        }
+        throw Exception("There is no common badge")
+    }
+
     fun part2(input: List<String>): Int {
-        return input.size
+        val group = arrayOf(listOf(' '), listOf(' '), listOf(' '))
+        var sum = 0
+
+        input.forEachIndexed { index, backpack ->
+            val distinctBackpack: List<Char> = backpack.toCharArray().distinct()
+            group[index % 3] = distinctBackpack
+
+
+            if (index % 3 == 2) {
+                sum += getItemPriority(findBadge(group))
+            }
+        }
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 157)
+    check(part2(testInput) == 70)
 
     val input = readInput("Day03")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
