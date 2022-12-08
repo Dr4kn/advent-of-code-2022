062376f8a88297e09c24ebd7d5c1ed7cf1f5f99b
Dr4kn <servicesDrkn@gmail.com>
2022-12-08 06:57:59 +0100

    part 1 done

--- src/day04/day04.kt
+++ src/day04/day04.kt
@@ -3,8 +3,36 @@ package day04
 import readInput
 
 fun main() {
+    fun findFullyContained(sections: Array<Int>): Int {
+        // the sections of the first elf fully contain the second one
+        if (sections[0] <= sections[2]) {
+            if (sections[1] >= sections[3]) {
+                return 1
+            }
+        }
+        // the second one fully contains the first
+        if (sections[2] <= sections[0]) {
+            if (sections[3] >= sections[1]) {
+                return 1
+            }
+        }
+
+        return 0
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+
+        input.forEach { sections ->
+            sum += findFullyContained(sections
+                    .split(',', '-')
+                    .map { section ->
+                        section.toInt()
+                    }
+                    .toTypedArray()
+            )
+        }
+        return sum
     }
 
     fun part2(input: List<String>): Int {
@@ -17,5 +45,5 @@ fun main() {
 
     val input = readInput("Day04")
     println(part1(input))
-    println(part2(input))
+//    println(part2(input))
 }
