040f257c4f4072d9126c3c9c9c4c8a2a06d2a51b
Dr4kn <servicesDrkn@gmail.com>
2022-12-10 05:27:30 +0100

    find Stack size

--- src/day05/day05.kt
+++ src/day05/day05.kt
@@ -1,8 +1,23 @@
+import java.lang.NumberFormatException
+
 fun main() {
+    fun numberOfStacks(input: List<String>): Int {
+        input.forEach { string ->
+            if (string[1].isDigit()) {
+                return (string.length / 4) + 1
+            }
+        }
+        return 0
+    }
+
+    fun parseInput(input: List<String>) {
+        print(numberOfStacks(input))
+    }
+
     fun part1(input: List<String>): String {
         // cargo
         // cargoCrane
-
+        parseInput(input)
         return "CMZ"
     }
 
@@ -12,10 +27,9 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
-    println(part1(testInput))
-//    check(part1(testInput) == "CMZ")
+    check(part1(testInput) == "CMZ")
 
     val input = readInput("Day05")
-    println(part1(input))
-    println(part2(input))
+//    println(part1(input))
+//    println(part2(input))
 }
