862ea3fa6711614230f2df14e9222f91d7713d39
Dr4kn <servicesDrkn@gmail.com>
2022-12-12 15:36:59 +0100

    setup day06

--- src/day06/day06.kt
+++ src/day06/day06.kt
@@ -1,21 +1,21 @@
 package day06
 
-import readInput
+import readInputAsString
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: String): Int {
+        return input.length
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: String): Int {
+        return input.length
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day06_test")
+    val testInput = readInputAsString("Day06_test")
     check(part1(testInput) == 7)
 
-    val input = readInput("Day06")
+    val input = readInputAsString("Day06")
     println(part1(input))
     println(part2(input))
 }
