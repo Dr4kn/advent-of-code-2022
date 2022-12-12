84a3dab0cd5156133fa2345e629dbae8f35ca6d6
Dr4kn <servicesDrkn@gmail.com>
2022-12-12 15:49:02 +0100

    part 1 done

--- src/day06/day06.kt
+++ src/day06/day06.kt
@@ -3,8 +3,13 @@ package day06
 import readInputAsString
 
 fun main() {
-    fun part1(input: String): Int {
-        return input.length
+    fun part1(input: String, windowSize: Int): Int {
+        input.windowed(windowSize, 1).forEachIndexed { index, window ->
+            if (window.toCharArray().distinct().size == windowSize) {
+                return index + windowSize
+            }
+        }
+        throw Exception("No distinct code found")
     }
 
     fun part2(input: String): Int {
@@ -13,9 +18,9 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInputAsString("Day06_test")
-    check(part1(testInput) == 7)
+    check(part1(testInput, 4) == 7)
 
     val input = readInputAsString("Day06")
-    println(part1(input))
-    println(part2(input))
+    println(part1(input, 4))
+//    println(part2(input))
 }
